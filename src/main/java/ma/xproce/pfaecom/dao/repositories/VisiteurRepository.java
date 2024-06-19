15a893d2866a5537ee4f618045cbccccbc0a1ae4
AYAAMINE <ayaamine63@gmail.com>
2024-06-19 21:51:53 +0100

    Version 1.1

--- src/main/java/ma/xproce/pfaecom/dao/repositories/VisiteurRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.xproce.pfaecom.dao.repositories;
-
-import jakarta.transaction.Transactional;
-import ma.xproce.pfaecom.dao.entities.Visiteur;
-import org.springframework.data.jpa.repository.JpaRepository;
-@Transactional
-
-public interface VisiteurRepository extends JpaRepository<Visiteur,Integer> {
-}
